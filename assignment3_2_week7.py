77c1127dede6c2bc7150e79641c83929817979dc
Jorge rea <reaj893@macomb.edu>
2022-10-10 21:12:52 -0400

    thrid in class assignment

---
+++ assignment3_2_week7.py
@@ -0,0 +1,11 @@
+
+
+for row in range(6):
+    print("#",end="")
+    #print some spaces
+    for spaces in range(row):
+        print(" ",end="")
+    print("#")
+
+#we nested a loop and the inside loop was dependant on the outer loop 
+
