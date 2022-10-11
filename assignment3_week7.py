a59890f393f179484dd47ddef9425504c08f04a3
Jorge rea <reaj893@macomb.edu>
2022-10-10 20:59:18 -0400

    started turtle but then started inclass assignment

---
+++ assignment3_week7.py
@@ -0,0 +1,47 @@
+#1st attempt - print 7 rows of 7 asterisks
+# for star in range (7):
+#    #print("*******")
+#     print("*"*7)
+
+# 2nd attempt - print decreasing each row by one star 
+for stars in range (7,0,-1):
+    print("*" * stars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("end of program")
---
+++ turtle_project2.py
@@ -0,0 +1,7 @@
+import turtle 
+from turtle import *
+
+
+
+
+
