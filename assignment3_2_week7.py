ab5a85dc00a9718a21f84612a73f44d54de7c610
Jorge rea <reaj893@macomb.edu>
2022-10-10 21:21:31 -0400

    while loops review

--- assignment3_2_week7.py
+++ assignment3_2_week7.py
@@ -8,4 +8,16 @@ for row in range(6):
     print("#")
 
 #we nested a loop and the inside loop was dependant on the outer loop 
+#doing the same problem in while loops
+row = 0 
+while row < 6: # range(6):
+    print("#",end="")
+    #print some spaces
+    spaces = 0
+    while spaces < row:
+        print(" ",end="")
+        spaces += 1
+    print("#")
+    row += 1    
+ 
 
